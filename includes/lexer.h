9865fbc83698dd9e0d44b8f5f3079189f3e0ce05
hadouweb <kengo14@hotmail.fr>
2016-11-22 14:57:37 +0100

    clean

--- includes/lexer.h
+++ includes/lexer.h
@@ -4,6 +4,23 @@
 # include <stdio.h>
 # include "libft.h"
 
+typedef struct		s_status
+{
+	unsigned int	prev;
+	unsigned int	curr;
+}					t_status;
+
+enum {
+	TK_BEG,
+	TK_COUNT,
+};
+
+enum {
+	STS_ACCEPT,
+	STS_REJECT,
+	STS_HUNGRY,
+};
+
 void	lex(char *str);
 
 #endif
--- srcs/lex.c
+++ srcs/lex.c
@@ -1,6 +1,22 @@
 #include "lexer.h"
 
+void	init_sts(t_status *statuses)
+{
+	int 	i;
+
+	i = 0;
+	while (i < TK_COUNT)
+	{
+		statuses[i].prev = STS_HUNGRY;
+		statuses[i].curr = STS_REJECT;
+		i++;
+	}
+}
+
 void	lex(char *str)
 {
+	t_status	statuses[TK_COUNT];
+
+	init_sts(statuses);
 	printf("%s\n", str);
 }
